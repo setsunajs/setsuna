0abcd766217fa7e8af7abdc6034f5ea50cca6e16
usagisah <usagisah@163.com>
2022-11-24 18:37:04 +0800

    add unmount

--- packages/setsuna/src/render.ts
+++ packages/setsuna/src/render.ts
@@ -2,6 +2,7 @@ import { def } from "@setsunajs/shared"
 import { error } from "./handler/errorHandler"
 import { isVNode } from "./jsx"
 import { patch } from "./patch/patch"
+import { unmount } from "./patch/unmount"
 import { SeElement, VNode } from "./runtime.type"
 
 const rootFlag = "__se_container__"
@@ -11,29 +12,37 @@ export function createRoot(container: HTMLElement) {
     throw error("render", "container is not a DOM node")
   }
 
-  if (rootFlag in container) {
+  if ((container as any)[rootFlag] !== rootFlag) {
     throw error(
       "render",
       "container has been created, please unmount app first"
     )
   }
 
-  def(container, rootFlag, true)
+  def(container, rootFlag, rootFlag)
 
+  let renderNode: VNode
   return {
     mount: (node: VNode | SeElement<any, any>) => {
       if (!isVNode(node)) {
         return error("render", "VNode is invalid")
       }
 
-      render(node, container)
+      render((renderNode = node), container)
     },
     hydrate: (node: VNode | SeElement<any, any>) => {
       if (!isVNode(node)) {
         return error("render", "VNode is invalid")
       }
 
-      hydrate(node, container)
+      hydrate((renderNode = node), container)
+    },
+    unmount: () => {
+      if (renderNode) {
+        unmount(renderNode)
+      }
+
+      ;(container as any)[rootFlag] = undefined
     }
   }
 }
