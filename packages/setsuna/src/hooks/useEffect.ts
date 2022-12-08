26a1bd7aa1f90b8509a089fed21189faed1f43e0
usagisah <usagisah@163.com>
2022-12-08 10:39:59 +0800

    fix useEffect not unSubscribe

--- packages/setsuna/src/hooks/useEffect.ts
+++ packages/setsuna/src/hooks/useEffect.ts
@@ -1,8 +1,13 @@
-import { Observable, SubOperator } from "@setsunajs/observable"
+import {
+  Observable,
+  SubOperator,
+  UnObservableSubscribe
+} from "@setsunajs/observable"
 import { resolveObservableState } from "@setsunajs/shared"
 import { isArray } from "@setsunajs/shared"
 import { error } from "../handler/errorHandler"
 import { HookState } from "../runtime.type"
+import { useMount } from "./lifecycle"
 
 export function useEffect<T>(
   observables: Array<Observable<T> | HookState<T>>,
@@ -16,14 +21,21 @@ export function useEffect<T>(
     )
   }
 
+  const unSubList: Array<{ unSub: UnObservableSubscribe; ob: Observable }> = []
   observables.forEach((value, index) => {
     const ob = resolveObservableState(value)
     ob
-      ? ob.subscribe(subscribe)
+      ? unSubList.push({ unSub: ob.subscribe(subscribe), ob })
       : error(
           "hook",
           `useEffect, observables item expected 'Observable | HookState', but in observables[${index}] got`,
           [ob, observables[index]]
         )
+  })
+
+  useMount(() => {
+    return () => {
+      unSubList.forEach(({ ob, unSub }) => !ob.closed && unSub())
+    }
   })
 }
